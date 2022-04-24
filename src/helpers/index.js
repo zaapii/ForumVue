aee428ba78423bb96c5cb92281ffc52b1100b7f3
unknown <zaapii@gmail.com>
2022-04-24 07:49:31 -0300

    destripando enmodulos

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,18 +1,35 @@
 export const findById = (resources, id) => {
-  if (!resources) return null
-  return resources.find((r) => r.id === id)
-}
+  if (!resources) return null;
+  return resources.find((r) => r.id === id);
+};
 
 export const upsert = (resources, resource) => {
-  const index = resources.findIndex((p) => p.id === resource.id)
+  const index = resources.findIndex((p) => p.id === resource.id);
   if (resource.id && index !== -1) {
-    resources[index] = resource
+    resources[index] = resource;
   } else {
-    resources.push(resource)
+    resources.push(resource);
   }
-}
+};
 
 export const docToResource = (doc) => {
-  if (typeof doc?.data !== 'function') return doc
-  return { ...doc.data(), id: doc.id }
-}
+  if (typeof doc?.data !== "function") return doc;
+  return { ...doc.data(), id: doc.id };
+};
+
+export const makeAppendChildToParentMutation = ({ parent, child }) => {
+  return (state, { childId, parentId }) => {
+    const resource = findById(state.items, parentId);
+    if (!resource) {
+      console.warn(
+        `Appending ${child} ${childId} to ${parent} ${parentId} failed because the parent didn't exist`
+      );
+      return;
+    }
+    resource[child] = resource[child] || [];
+
+    if (!resource[child].includes(childId)) {
+      resource[child].push(childId);
+    }
+  };
+};
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="container">
-    <h1>Profile</h1>
-    <!-- <div class="flex-grid">
+  <div class="container" style="width: 100%">
+    <div class="flex-grid">
       <div class="col-3 push-top">
         <UserProfileCard v-if="!edit" :user="user" />
-
-        <UserProfileCardEditor v-if="edit" :user="user" />
+        <UserProfileCardEditor v-else :user="user" />
       </div>
 
       <div class="col-7 push-top">
@@ -13,56 +11,30 @@
           <span class="text-lead"> {{ user.username }} recent activity </span>
           <a href="#">See only started threads?</a>
         </div>
-
         <hr />
-
-        <posts-list :posts="user.posts" /> -->
-
-    <!--  <div class="activity-list">
-                  <div class="activity">
-                      <div class="activity-header">
-                          <img src="https://i.imgur.com/OqlZN48.jpg" alt="" class="hide-mobile avatar-small">
-                          <p class="title">
-                              How can I chop onions without crying?
-                              <span>Joker started a topic in Cooking</span>
-                          </p>
-
-                      </div>
-
-                      <div class="post-content">
-                        <div>
-                          <p>I absolutely love onions, but they hurt my eyes! Is there a way where you can chop onions without crying?</p>
-                        </div>
-                      </div>
-
-                      <div class="thread-details">
-                          <span>4 minutes ago</span>
-                          <span>1 comments</span>
-                      </div>
-                  </div>
-              </div> -->
-    <!--     </div>
-    </div> -->
+        <PostsList :posts="user.posts" />
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-/* import PostsList from "@/components/PostsList.vue"; */
-import { mapGetters } from 'vuex'
-/* import UserProfileCard from "@/components/UserProfileCard";
-import UserProfileCardEditor from "@/components/UserProfileCardEditor"; */
+import PostsList from "@/components/PostsList";
+import UserProfileCard from "@/components/UserProfileCard";
+import UserProfileCardEditor from "@/components/UserProfileCardEditor";
+import { mapGetters } from "vuex";
+import asyncDataStatus from "@/mixins/asyncDataStatus";
 export default {
-  /*     components: { PostsList, UserProfileCard, UserProfileCardEditor }, */
-  computed: {
-    ...mapGetters({ user: 'authUser' })
-  },
+  components: { PostsList, UserProfileCard, UserProfileCardEditor },
+  mixins: [asyncDataStatus],
   props: {
-    edit: {
-      type: Boolean,
-      default: false
-    }
-  }
-}
+    edit: { type: Boolean, default: false },
+  },
+  computed: {
+    ...mapGetters({ user: "authUser" }),
+  },
+  async created() {
+    await this.$store.dispatch("fetchAuthUsersPosts");
+    this.asyncDataStatus_fetched();
+  },
+};
 </script>
-
-<style></style>
