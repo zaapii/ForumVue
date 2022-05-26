9819d2ffaeb48163b488401d249dd76b6d088b0d
unknown <zaapii@gmail.com>
2022-05-26 01:05:52 -0300

    test build4

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID
+  apiKey:"AIzaSyDoBxsA-4DGI38XLTcgIP9Y4nWo0-a9_xg",
+  authDomain:"vue-forum-6e786.firebaseapp.com",
+  projectId:"vue-forum-6e786",
+  storageBucket:"vue-forum-6e786.appspot.com",
+  messagingSenderId:"267527989840",
+  appId:"1:267527989840:web:08cdb81e53415e4314ff55"
 }
